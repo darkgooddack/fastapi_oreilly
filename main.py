c4b8ac4a831718eb13ba93796201d83dfcab68d3
darkgooddack <ulyanaworkpage@gmail.com>
2024-12-26 17:40:43 +0300

    Написан сервисный уровень и unit tests

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import web.creature
 app = FastAPI()
 
 app.include_router(web.explorer.router)
-app.include_router(web.explorer.router)
+app.include_router(web.creature.router)
 
 if __name__ == "__main__":
     import uvicorn
